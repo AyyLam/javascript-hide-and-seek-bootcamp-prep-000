7470a25acac8929f9537c6e3175dfb352c34ff24
Anthony Lam <al3101@nyu.edu>
2018-04-29 20:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var deepestGrandNode = document.querySelector('div#grand-node');
-  var grandNodeChid = deepestGrandNode[0]
-  for (let i = 0; i < deepestGrandNode.length; i++){
-    grandNodeChid = grandNodeChid[i]
+  var grandNodeChild = deepestGrandNode[0]
+  for (let i = 0; grandNodeChild.child ===undefined; i++){
+    grandNodeChild = grandNodeChild[i]
   }
-  return grandNodeChid
+  return grandNodeChild
 } 
 
