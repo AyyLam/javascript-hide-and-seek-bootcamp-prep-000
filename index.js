85494e1facdd2bfeca7048db4bf15b518f84947e
Anthony Lam <al3101@nyu.edu>
2018-04-29 21:08:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranked = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranked.length; i++) {
+    ranked[i].innerHTML = parseInt(ranked[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var deepestGrandNode = document.querySelector('#grand-node');
+  var grandNodeChild = deepestGrandNode.children[0];
+  
+  while (grandNodeChild.children[0]) {
+    grandNodeChild = grandNodeChild.children[0]
+  }
+  return grandNodeChild;
+} 
+
