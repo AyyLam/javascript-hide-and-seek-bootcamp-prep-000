700a090cbb9c9043eb42b3fda6d18db92b31f1be
Anthony Lam <al3101@nyu.edu>
2018-04-29 20:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var deepestGrandNode = document.querySelector('#grand-node');
+  var deepestGrandNode = document.querySelector('div#grand-node');
   while (deepestGrandNode.child) {
     deepestGrandNode = deepestGrandNode.deepestChild
   }
